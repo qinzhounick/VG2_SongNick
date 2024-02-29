cd54675a6cf21cbb516c06a99346bdd9aa0d0846
qinzhounick <qinzhounick@wustl.edu>
2024-02-28 21:06:14 -0600

    Q5 Complete

---
+++ Assets/Code/Q5/Enemy.cs
@@ -0,0 +1,67 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.AI;
+
+
+namespace FPS
+{
+    public class Enemy : MonoBehaviour
+    {
+        NavMeshAgent navAgent;
+        public Transform priorityTarget;
+        public Transform target;
+        public Transform patrolRoute;
+
+        int patrolIndex;
+        public float chaseDistance;
+
+        // Start is called before the first frame update
+        void Start()
+        {
+            navAgent = GetComponent<NavMeshAgent>();
+        }
+
+        // Update is called once per frame
+        void Update()
+        {
+            if (patrolRoute)
+            {
+                target = patrolRoute.GetChild(patrolIndex);
+
+                float distance = Vector3.Distance(transform.position, target.position);
+                print("Distance: " +  distance);
+
+                if(distance <= 1.5f)
+                {
+                    patrolIndex++;
+                    if(patrolIndex >= patrolRoute.childCount)
+                    {
+                        patrolIndex = 0;
+                    }
+                }
+            }
+
+            if (priorityTarget)
+            {
+                float priorityTargetDistance = Vector3.Distance(transform.position, priorityTarget.position);
+
+                if(priorityTargetDistance <= chaseDistance)
+                {
+                    target = priorityTarget;
+                    GetComponent<Renderer>().material.color = Color.red;
+                }
+                else
+                {
+                    GetComponent<Renderer>().material.color = Color.white;
+                }
+            }
+
+            if (target)
+            {
+                navAgent.SetDestination(target.position);
+            }
+        }
+    }
+}
+
