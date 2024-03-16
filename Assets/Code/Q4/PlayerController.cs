3030aa49a9aff2e5e25c0dfcf0bd4d7fdde8c2b7
qinzhounick <qinzhounick@wustl.edu>
2024-03-16 15:54:35 -0500

    Q6 Complete

--- Assets/Code/Q4/PlayerController.cs
+++ Assets/Code/Q4/PlayerController.cs
@@ -9,43 +9,59 @@ namespace FPS{
     {
         public static PlayerController instance;
 
+        public Transform povOrigin;
+        public Transform projectileOrigin;
+        public GameObject projectilePrefab;
+
+        public float attackRange;
+
         public List<int> keyIdsObtained;
         private void Awake()
         {
             instance = this;
             keyIdsObtained = new List<int>();
         }
-        // Update is called once per frame
-        void Update()
+
+        void OnPrimaryAttack()
         {
-            Keyboard keyboardInput = Keyboard.current;
-            Mouse mouseInput = Mouse.current;
-            if (keyboardInput != null && mouseInput != null)
+            RaycastHit hit;
+            bool hitSomething = Physics.Raycast(povOrigin.position, povOrigin.forward, out hit, attackRange);
+            if (hitSomething)
             {
-                if(keyboardInput.eKey.wasPressedThisFrame)
+                Rigidbody targetRigidbody = hit.transform.GetComponent<Rigidbody>();
+                if (targetRigidbody)
                 {
-                    Vector2 mousePosition = mouseInput.position.ReadValue();
-
-                    Ray ray = Camera.main.ScreenPointToRay(mousePosition);
-                    RaycastHit hit;
-                    if(Physics.Raycast(ray, out hit, 2f))
-                    {
-                        print("Interacted with " + hit.transform.name + " from " + hit.distance + "m.");
-
-                        Door targetDoor = hit.transform.GetComponent<Door>();
-                        if (targetDoor)
-                        {
-                            targetDoor.Interact();
-                        }
-
-                        InteractButton targetButton = hit.transform.GetComponent<InteractButton>();
-                        if (targetButton != null)
-                        {
-                            targetButton.Interact();
-                        }
-                    
-                    }
+                    targetRigidbody.AddForce(povOrigin.forward * 100f, ForceMode.Impulse);
                 }
+            }
+        }
+
+        void OnSecondaryAttack()
+        {
+            GameObject projectile = Instantiate(projectilePrefab, projectileOrigin.position, Quaternion.LookRotation(povOrigin.forward));
+            projectile.transform.localScale = Vector3.one * 5f;
+            projectile.GetComponent<Rigidbody>().AddForce(povOrigin.forward * 25f, ForceMode.Impulse);
+        }
+
+        void OnInteract()
+        {
+            RaycastHit hit;
+            if (Physics.Raycast(povOrigin.position, povOrigin.forward, out hit, 2f))
+            {
+                //print("Interacted with " + hit.transform.name + " from " + hit.distance + "m.");
+
+                Door targetDoor = hit.transform.GetComponent<Door>();
+                if (targetDoor)
+                {
+                    targetDoor.Interact();
+                }
+
+                InteractButton targetButton = hit.transform.GetComponent<InteractButton>();
+                if (targetButton != null)
+                {
+                    targetButton.Interact();
+                }
+
             }
         }
     }
